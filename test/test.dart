10c4bd71c49c76dfcd90a2a487b9d68a1e7e8462
kimkyungwon <openwind125@gmail.com>
2022-03-22 17:53:05 +0900

    Add get random data

--- test/test.dart
+++ test/test.dart
@@ -1,1 +1,11 @@
-void main() {}
+import 'package:flutter_test/flutter_test.dart';
+import 'package:meal/services/api.dart';
+
+void main() async {
+  var random = await getRandomMealData();
+  print(random.lists?[0].strMeal);
+
+  test('check random data', () {
+    expect(random.lists?[0].strMeal, isNotNull);
+  });
+}
